9bbd4dafa3bb83537797101f5775934340ec339b
Ducbb.uv.coder <ducbbth04935@gmail.com>
2025-07-15 09:51:20 +0700

    gộp css

--- nguoidung/nguoidung.js
+++ nguoidung/nguoidung.js
@@ -1,60 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-	if (!currentUser) {
-		localStorage.setItem("showLoginPopup", "true");
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+// 	if (!currentUser) {
+// 		localStorage.setItem("showLoginPopup", "true");
 
-		document.body.innerHTML = "";
-		setTimeout(() => {
-			window.location.replace("nguoidung.html");
-		}, 100);
-	} else {
-		document.getElementById("welcome").textContent = `Xin chào ${
-			currentUser.username || "người dùng"
-		}`;
-	}
-});
+// 		document.body.innerHTML = "";
+// 		setTimeout(() => {
+// 			window.location.replace("nguoidung.html");
+// 		}, 100);
+// 	} else {
+// 		document.getElementById("welcome").textContent = `Xin chào ${
+// 			currentUser.username || "người dùng"
+// 		}`;
+// 	}
+// });
 
-function logout() {
-	localStorage.removeItem("currentUser");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// xoá tạm thời để tránh ko load đc trang
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-	document.body.classList.remove("fade-in");
-	document.body.classList.add("fade-out");
+// document.addEventListener("DOMContentLoaded", () => {
+// 	document.body.classList.add("fade-in");
+// });
 
-	setTimeout(() => {
-		window.location.href = "nguoidung.html";
-	}, 800);
-}
+// document.querySelectorAll("button[data-href]").forEach((btn) => {
+// 	btn.addEventListener("click", function (e) {
+// 		e.preventDefault();
+// 		const href = btn.getAttribute("data-href");
 
-document.addEventListener("DOMContentLoaded", () => {
-	document.body.classList.add("fade-in");
-});
+// 		if (href) {
+// 			document.body.classList.remove("fade-in");
+// 			document.body.classList.add("fade-out");
 
-document.querySelectorAll("button[data-href]").forEach((btn) => {
-	btn.addEventListener("click", function (e) {
-		e.preventDefault();
-		const href = btn.getAttribute("data-href");
+// 			setTimeout(() => {
+// 				window.location.href = href;
+// 			}, 800);
+// 		}
+// 	});
+// });
 
-		if (href) {
-			document.body.classList.remove("fade-in");
-			document.body.classList.add("fade-out");
-
-			setTimeout(() => {
-				window.location.href = href;
-			}, 800);
-		}
-	});
-});
-
-document.querySelectorAll("a[href]").forEach((link) => {
-	const href = link.getAttribute("href");
-	if (href && !href.startsWith("#") && !link.hasAttribute("target")) {
-		link.addEventListener("click", function (e) {
-			e.preventDefault();
-			document.body.classList.remove("fade-in");
-			document.body.classList.add("fade-out");
-			setTimeout(() => {
-				window.location.href = href;
-			}, 800);
-		});
-	}
-});
+// document.querySelectorAll("a[href]").forEach((link) => {
+// 	const href = link.getAttribute("href");
+// 	if (href && !href.startsWith("#") && !link.hasAttribute("target")) {
+// 		link.addEventListener("click", function (e) {
+// 			e.preventDefault();
+// 			document.body.classList.remove("fade-in");
+// 			document.body.classList.add("fade-out");
+// 			setTimeout(() => {
+// 				window.location.href = href;
+// 			}, 800);
+// 		});
+// 	}
+// });
---
+++ thongtinrieng/thongtinrieng.js
@@ -0,0 +1,10 @@
+// function logout() {
+// 	localStorage.removeItem("currentUser");
+
+// 	document.body.classList.remove("fade-in");
+// 	document.body.classList.add("fade-out");
+
+// 	setTimeout(() => {
+// 		window.location.href = "nguoidung.html";
+// 	}, 800);
+// }
